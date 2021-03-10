3de838e561c00b35b19ce20b2b464dbadd90f071
Jacob Kohl <jkohl7@my.wctc.edu>
2021-03-09 21:23:32 -0600

    Discounts are added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+  
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+using System.Linq;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NorthwindContext _northwindContext;
+        public HomeController(NorthwindContext db) => _northwindContext = db;
+
+        public ActionResult Index() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
+    }
+}
